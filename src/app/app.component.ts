506bab5a959361cdfc8ec901dec949182c183a68
Brian Pilati <brianpilati@cos.io>
2025-06-06 11:19:20 -0500

    Added a Brian Pilati example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationComponent} from './navigation/navigation.component';
+import { BPWeatherComponent } from "./brian-pilati/weather/weather.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, BPWeatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {WeatherComponent} from './weather/weather.component';
+import { BPWeatherComponent } from './brian-pilati/weather/weather.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,23 @@ export const routes: Routes = [
         path: 'weather',
         component: WeatherComponent,
         title: "This is a new page title",
+        children: [
+          {
+            path: ':id',
+            component: WeatherComponent,
+            children: [
+              {
+                path: 'edit',
+                component: WeatherComponent
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: 'brian-pilati',
+        component: BPWeatherComponent,
+        title: "Brian Pilati Weather",
         children: [
           {
             path: ':id',
