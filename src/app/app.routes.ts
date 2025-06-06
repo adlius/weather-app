8ddc9d06bbed631315926bc6fe2a55a6fabba72f
Oleh Paduchak <opaduchak@exoft.net>
2025-06-06 19:08:05 +0300

    routing gimmicks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,33 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { WeatherComponent } from './weather/weather.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {WeatherComponent} from './weather/weather.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'weather',
-    component: WeatherComponent,
+    path: '',
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        title: "This is a home title",
+      },
+      {
+        path: 'weather',
+        component: WeatherComponent,
+        title: "This is a new page title",
+        children: [
+          {
+            path: ':id',
+            component: WeatherComponent,
+            children: [
+              {
+                path: 'edit',
+                component: WeatherComponent
+              }
+            ]
+          }
+        ]
+      },
+    ]
   },
 ];
