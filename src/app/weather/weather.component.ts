fad9b7227b8f6a46656fdc1a77eed56223e71f21
Oleh Paduchak <opaduchak@exoft.net>
2025-06-20 19:02:55 +0300

    added event handlers

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { citiesData } from './data/cities_data_ts';
-import { todayForecast, forecastTestData } from './data/forecast.data';
+import {Component, OnInit, signal} from '@angular/core';
+import {citiesData} from './data/cities_data_ts';
+import {todayForecast, forecastTestData} from './data/forecast.data';
 
 @Component({
   selector: 'app-weather',
@@ -9,12 +9,23 @@ import { todayForecast, forecastTestData } from './data/forecast.data';
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent implements OnInit {
-  city_search = ('@input')
+  city_search ='';
+
+
   ngOnInit() {
     // var city_search = $
-    citiesData.filter()
+    // citiesData.filter()
     console.log(citiesData);
     console.log(todayForecast);
     console.log(forecastTestData);
+  }
+
+  handleSearch() {
+    console.log("pressed");
+  }
+
+  updateQuery(event: Event) {
+    console.log(event)
+    // this.city_search = event.;
   }
 }
